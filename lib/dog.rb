fb229e0df59c01a7621745d772f15ad7bd44261d
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-08 10:14:29 +0300

    Assignment completed

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class Dog 
+    # class body
+
+    # instance method definition
+    def bark
+        puts "Woof!"
+    end
+        # instance method definition
+    def sit
+        puts "The Dog is sitting" 
+    end
+end 
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person 
+    # class body
+
+    # instance method
+    def talk
+        puts "Hello World!"
+    end
+    # instance method
+    def walk
+        puts "The Person is walking"
+    end
+end
